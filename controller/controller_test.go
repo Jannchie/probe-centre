ff567c3b0e9e57a8f9110857a131fe3269b4e885
Jannchie <jannchie@gmail.com>
2021-04-05 20:27:24 +0900

    feat: add user tests

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 
 	"github.com/Jannchie/probe-centre/db"
+
+	"github.com/Jannchie/probe-centre/service"
+
 	"github.com/Jannchie/probe-centre/model"
 
 	"github.com/Jannchie/probe-centre/test"
@@ -17,6 +20,8 @@ func TestMain(m *testing.M) {
 		Mail:  "test@test.com",
 		Token: "00000000-0000-0000-0000-000000000000",
 	}
+	passwd := "123456"
+	user.Key, user.Salt = service.GenerateKeyAndSalt(passwd)
 	db.DB.Create(&user)
 	os.Exit(m.Run())
 }
--- controller/user_test.go
+++ controller/user_test.go
@@ -12,23 +12,85 @@ import (
 
 func TestCreateUser(t *testing.T) {
 	gin.SetMode(gin.ReleaseMode)
+	dataStr := "{\"Mail\": \"test@test.com\",\"Password\":\"123456\"}"
+	w := testHandle(CreateUser, dataStr)
+	assert.Equal(t, 400, w.Code)
+	dataStr = "{\"Mail\": \"test1@test.com\",\"Password\":\"123456\"}"
+	w = testHandle(CreateUser, dataStr)
+	assert.Equal(t, 200, w.Code)
+	dataStr = "{\"Mail\": \"test1@test.com\",\"Password\":\"123456\"}"
+	w = testHandle(CreateUser, dataStr)
+	assert.Equal(t, 400, w.Code)
+	dataStr = "1{\"Mail\": \"test1@test.com\".\"Password\":\"123456\"}"
+	w = testHandle(CreateUser, dataStr)
+	assert.Equal(t, 400, w.Code)
+}
+func testHandle(handle func(c *gin.Context), dataStr string) *httptest.ResponseRecorder {
 	r := gin.New()
-	r.POST("/test", CreateUser)
+	r.POST("/test", handle)
 	w := httptest.NewRecorder()
-	data := bytes.NewReader([]byte("{\"Mail\": \"test@test.com\",\"Password\":\"123456\"}"))
+	data := bytes.NewReader([]byte(dataStr))
 	req, _ := http.NewRequest("POST", "/test", data)
 	r.ServeHTTP(w, req)
-	assert.Equal(t, 400, w.Code)
-
-	w = httptest.NewRecorder()
-	data = bytes.NewReader([]byte("{\"Mail\": \"test1@test.com\",\"Password\":\"123456\"}"))
-	req, _ = http.NewRequest("POST", "/test", data)
+	return w
+}
+func testHandleWithToken(handle func(c *gin.Context), dataStr string, token string) *httptest.ResponseRecorder {
+	r := gin.New()
+	r.POST("/test", handle)
+	w := httptest.NewRecorder()
+	data := bytes.NewReader([]byte(dataStr))
+	req, _ := http.NewRequest("POST", "/test", data)
+	req.Header.Add("token", token)
 	r.ServeHTTP(w, req)
+	return w
+}
+
+func TestGetUser(t *testing.T) {
+	w := testHandle(GetUser, `{"Token":"00000000-0000-0000-0000-000000000000"}`)
 	assert.Equal(t, 200, w.Code)
+	w = testHandle(GetUser, `{"Token":"00000000-0000-0000-0000-000000000001"}`)
+	assert.Equal(t, 400, w.Code)
+	w = testHandle(GetUser, `{"ID":1}`)
+	assert.Equal(t, 200, w.Code)
+	w = testHandle(GetUser, `{"ID":0}`)
+	assert.Equal(t, 400, w.Code)
+	w = testHandle(GetUser, `{"ID":100}`)
+	assert.Equal(t, 400, w.Code)
+	w = testHandle(GetUser, `{"other":123}`)
+	assert.Equal(t, 400, w.Code)
+	w = testHandle(GetUser, `123123`)
+	assert.Equal(t, 400, w.Code)
+}
 
-	w = httptest.NewRecorder()
-	data = bytes.NewReader([]byte("{\"Password\":\"123456\"}"))
-	req, _ = http.NewRequest("POST", "/test", data)
-	r.ServeHTTP(w, req)
+func TestUpdateUser(t *testing.T) {
+	token := "00000000-0000-0000-0000-000000000000"
+	w := testHandleWithToken(UpdateUser, "", token)
+	assert.Equal(t, 400, w.Code)
+	w = testHandleWithToken(UpdateUser, `{"Name": "Temp"}`, token)
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestGetMe(t *testing.T) {
+	token := "00000000-0000-0000-0000-000000000000"
+	w := testHandleWithToken(GetMe, "", token)
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestRefreshToken(t *testing.T) {
+	token := "00000000-0000-0000-0000-000000000000"
+	w := testHandleWithToken(RefreshToken, "", token)
+	assert.Equal(t, 200, w.Code)
+	w = testHandleWithToken(RefreshToken, "", token)
+	assert.Equal(t, 400, w.Code)
+}
+
+func TestLogin(t *testing.T) {
+	w := testHandle(Login, `{"Mail":"test@test.com","Password":"123456"}`)
+	assert.Equal(t, 200, w.Code)
+	w = testHandle(Login, `{"Mail1":"test@test.com","Password":"123456"}`)
+	assert.Equal(t, 400, w.Code)
+	w = testHandle(Login, `{"Mail":"test@test.com","Password":"1234567"}`)
+	assert.Equal(t, 400, w.Code)
+	w = testHandle(Login, `{"Mail":"test@tes1t.com","Password":"123456"}`)
 	assert.Equal(t, 400, w.Code)
 }
